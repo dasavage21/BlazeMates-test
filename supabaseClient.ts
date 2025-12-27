3e4eee0f84b0e2a91994cdb71e6dcde4c43401b3
ben <81379248+dasavage21@users.noreply.github.com>
2025-12-26 19:53:20 -0500

    Updated supabaseClient.ts

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -6,11 +6,13 @@ const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL!;
 const SUPABASE_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
 export const SUPABASE_PROJECT_REF = SUPABASE_URL.replace(/^https?:\/\//, '').split('.')[0];
 
+const isServer = typeof window === 'undefined';
+
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
-    storage: AsyncStorage,
+    storage: isServer ? undefined : AsyncStorage,
     autoRefreshToken: true,
-    persistSession: true,
+    persistSession: !isServer,
     detectSessionInUrl: false,
   },
 });
