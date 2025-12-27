57a0b77647f301d3e227b37bb56d2bbcbcff561f
ben <81379248+dasavage21@users.noreply.github.com>
2025-12-26 19:19:41 -0500

    Updated map.tsx

--- app/map.tsx
+++ app/map.tsx
@@ -1,12 +1,5 @@
 // © 2025 Benjamin Hawk. All rights reserved.
 
-import { Platform } from 'react-native';
-import MapWeb from '../components/MapWeb';
 import MapNative from '../components/MapNative';
 
-const MapScreen = Platform.select({
-  web: MapWeb,
-  default: MapNative,
-});
-
-export default MapScreen;
+export default MapNative;
---
+++ components/MapNative.web.tsx
@@ -0,0 +1,5 @@
+// © 2025 Benjamin Hawk. All rights reserved.
+
+import MapWeb from './MapWeb';
+
+export default MapWeb;
