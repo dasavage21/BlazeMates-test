af81ae239f0f7fb0d0f165ed80d092c6f78d6a06
ben <81379248+dasavage21@users.noreply.github.com>
2025-12-26 19:48:02 -0500

    Updated supabaseClient.ts

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,10 +1,9 @@
 // supabaseClient.ts
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
-import Constants from 'expo-constants';
 
-const SUPABASE_URL = Constants.expoConfig?.extra?.EXPO_PUBLIC_SUPABASE_URL || process.env.EXPO_PUBLIC_SUPABASE_URL || '';
-const SUPABASE_ANON_KEY = Constants.expoConfig?.extra?.EXPO_PUBLIC_SUPABASE_ANON_KEY || process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || '';
+const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL!;
+const SUPABASE_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
 export const SUPABASE_PROJECT_REF = SUPABASE_URL.replace(/^https?:\/\//, '').split('.')[0];
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
