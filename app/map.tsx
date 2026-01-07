ad7edc73f75e8c168ae9a914bb0e13d20f4d72fb
ben <81379248+dasavage21@users.noreply.github.com>
2026-01-06 19:52:03 -0500

    Updated package-lock.json

--- app/map.tsx
+++
@@ -1,5 +0,0 @@
-// © 2025 Benjamin Hawk. All rights reserved.
-
-import MapNative from '../components/MapNative';
-
-export default MapNative;
--- components/MapNative.web.tsx
+++
@@ -1,5 +0,0 @@
-// © 2025 Benjamin Hawk. All rights reserved.
-
-import MapWeb from './MapWeb';
-
-export default MapWeb;
